d44b71887da8a48318a9882202117ab3b9c8cfe5
Jeremy Cerise <jcerise06@gmail.com>
2012-12-11 16:27:03 -0700

    Added a js theme file for theme, added jquery and theme functions to base.html

---
+++ static/mustache-theme-functions.js
@@ -0,0 +1,9 @@
+(function($) {
+
+	$(".date").each(function() {
+    	var dateTime = $(this).text();
+		alert(dateTime);
+      	//$("time", this).html(getBinaryTime(postDate));
+    });
+
+})(jQuery);
